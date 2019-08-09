86f584a50917287daa303c2f61ed1775e6ecb9ce
till <veganeslama@gmail.com>
2019-08-09 10:59:17 +0200

    Test

--- SpotifyToMp3/download.js
+++ SpotifyToMp3/download.js
@@ -56,7 +56,7 @@ let getTracks = function (spotify_url) {
         console.log(e)
     }
     fetch(
-        'api/spotify/playlist?id=' + spotify_url
+        'https://s.chulte.de/spotify2mp3/api/spotify/playlist?id=' + spotify_url
     ).then(
         function (response) {
             return response.text()
@@ -78,7 +78,7 @@ let getTracks = function (spotify_url) {
 
             function getStreamUrl(array, callback) {
                 let title = array[0];
-                fetch('api/youtube/stream', {
+                fetch('https://s.chulte.de/spotify2mp3/api/youtube/stream', {
                     method: 'POST',
                     body: title,
                     headers: {
@@ -123,7 +123,7 @@ let getTracks = function (spotify_url) {
                         if (streams[count] === '') {
                             callback()
                         } else {
-                            fetch('api/youtube/download', {
+                            fetch('https://s.chulte.de/spotify2mp3/api/youtube/download', {
                                 method: 'POST',
                                 body: streams[count]
                             })
